db32ae3e3a54708e81af95dc67826b3a3e38939d
lee0798 <71830104+lee0798@users.noreply.github.com>
2023-04-29 00:07:28 +0900

    project day1 code
    learning theory of JS, using class and click event

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
-console.dir
+const title=document.querySelector("div.hello:first-child h1");
+function handleTitleClick(){
+    title.style.color ="blue";
+    console.log("title was clicked");
+}
+title.addEventListener("click",handleTitleClick);
+
 
 
 
