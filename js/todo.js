3963988a3632acc7c51645a2bf9c2e3b435645c3
HANA <leehannah1334@naver.com>
2023-05-06 16:30:00 +0900

    commit for todo-list

--- js/todo.js
+++ js/todo.js
@@ -13,8 +13,6 @@ function saveToDos() {
 function deleteToDo(event) {
   const li = event.target.parentElement;
   li.remove();
-  toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
-  saveToDos();
 }
 
 function paintToDo(newTodo) {
