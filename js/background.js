b41c2aba9b99915cce2173d282553c2180b9dc23
Lee Seung-hyun <127467249+shbee12@users.noreply.github.com>
2023-05-10 00:06:10 +0900

    Change about background
    Change about background

--- js/background.js
+++ js/background.js
@@ -8,14 +8,18 @@ bgImage.src = `image/${chosenImage}`;
 
 document.body.appendChild(bgImage);*/
 const images = [
-    "0.jpeg"
+    "0.jpg","1.jpg","2.jpg","3.jpg","4.jpg","5.jpg",
+    "6.jpg","7.jpg","8.jpg","9.jpg","10.jpg",
+    "11.jpg","12.jpg","13.jpg","14.jpg","15.jpg",
+    "16.jpg","17.jpg","18.jpg","19.jpg","20.jpg",
+    "21.jpg","22.jpg","23.jpg","24.jpg","25.jpg",
   ]
-const modalImage = document.getElementById('modal_box')
-function changeQuote1(){
+const modalImage = document.getElementById('body')
+function changePic(){
     const choseImage = images[Math.floor(Math.random() * images.length)];
     
     modalImage.style.backgroundImage = "url(img/"+choseImage+")";
     console.log(choseImage);
     }
   
-    window.addEventListener('load', changeQuote1)
+    window.addEventListener('load', changePic)
