e19244bbf30125a8d51f1e943a994b5daa256c01
Lee Seung-hyun <127467249+shbee12@users.noreply.github.com>
2023-05-14 22:24:14 +0900

    Update youtube.js

--- js/youtube.js
+++ js/youtube.js
@@ -4,7 +4,23 @@ const youtubemusic = [
     "https://www.youtube.com/embed/fz2KhVErjqs",
     "https://www.youtube.com/embed/TXWntRQcpK0",
     "https://www.youtube.com/embed/mipIdvWyGZA",
+    
     "https://www.youtube.com/embed/hdbrc6nl7OM",
+    "https://www.youtube.com/embed/BMW5TB0weSs",
+    "https://www.youtube.com/embed/Zdzwer-xoiQ",
+    "https://www.youtube.com/embed/H3p7JUZiet0",
+    "https://www.youtube.com/embed/UHwJFkfqaaI",
+    
+    "https://www.youtube.com/embed/nd5GMzxrGYs",
+    "https://www.youtube.com/embed/ZVCLJTYqU_8",
+    "https://www.youtube.com/embed/S7XLVTMHRmQ",
+    "https://www.youtube.com/embed/0K-x_hdW36I",
+    "https://www.youtube.com/embed/QMSv28VwWwY",
+    
+    "https://www.youtube.com/embed/HzG2PeKJIF4",
+    "https://www.youtube.com/embed/TnR0D8_4iGk",
+    "https://www.youtube.com/embed/m6mAIF-D4L0",
+
 ];
 
 function changeIframeUrl(){
