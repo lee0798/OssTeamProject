f8efae932c78b02e56084df594c89a934ddde5fb
이하나 <127467192+LeEHaNa1@users.noreply.github.com>
2023-05-25 00:17:56 +0900

    To Show dates clearly
    캘린더 날짜 잘 안 보이는 것 때문에 div 만들어서 일단 덧대놨어요! 만들기만 한 거라서 blur처리만 한 상태입니다.

--- js/calender.js
+++ js/calender.js
@@ -50,16 +50,18 @@ const makeCalendar = (item) =>{
         i === today?
         htmlDummy += `<div onclick="changeTitle(${i}, ${currnetYear}, ${currnetMonth})">
         ${`<span style="color:red">${i}</span>`}
-        <p>
+        <div style="background-color: #c0baba71;backdrop-filter: blur(3px);position: absolute; left: 0%; right: 0%; top:0%; min-height: 1.2rem; padding: 6px; z-index: -1;">
+        <p style="z-index: 1">
         ${calendarItem(date)}
-        </p>
+        </p></div>
         </div>`
         :
         htmlDummy += `<div onclick="changeTitle(${i}, ${currnetYear}, ${currnetMonth})">
         ${`<span>${i}</span>`}
-        <p style="color:red"; >
+        <div style="background-color: #c0baba71;backdrop-filter: blur(3px);position: absolute; left: 0%; right: 0%; top:0%; min-height: 1.2rem; padding: 6px; z-index: -1;">
+        <p style="color:red; z-index: 1"; >
         ${calendarItem(date)}
-        </p>
+        </p></div>
         </div>`
 
         
